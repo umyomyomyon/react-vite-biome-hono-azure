a922370b09b894c5ee5112156a2adcdaf4a21daf
umyomyomyon <minecraft.sentry.yym@gmail.com>
2024-09-24 22:07:45 +0900

    Revert "backendディレクトリに変更を入れてみる"
    This reverts commit e2ded13e642efb5a867ecbaedb70bde8923f1028.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,6 @@
 import { Hono } from "hono";
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Azure Functions! Hoge"));
+app.get("/", (c) => c.text("Hello Azure Functions!"));
 
 export default app;
