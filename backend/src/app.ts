e2ded13e642efb5a867ecbaedb70bde8923f1028
umyomyomyon <minecraft.sentry.yym@gmail.com>
2024-09-24 22:00:45 +0900

    backendディレクトリに変更を入れてみる

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,6 @@
 import { Hono } from "hono";
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Azure Functions!"));
+app.get("/", (c) => c.text("Hello Azure Functions! Hoge"));
 
 export default app;
