ed3c978853c5fc3581772657646ea717aa073b77
umyomyomyon <minecraft.sentry.yym@gmail.com>
2024-09-25 14:29:18 +0900

    Revert "hoge"
    This reverts commit 5d1e5114c887cf112f729103f36f3338c7478ddf.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
-// const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:7071";
+const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:7071";
 
 export const fetchApi = async () => {
-	const response = await fetch("/api/hello");
+	const response = await fetch(`${apiUrl}/hello`);
 	return response.text();
 };
